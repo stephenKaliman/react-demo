2fb9f9f00ad5504faf39b5cae8837014d2a8d72c
Stephen Kelman <stephenrkelman@gmail.com>
2021-12-21 09:32:21 -0800

    Step 7.1: Component-specific styling

--- demo-app/src/components/Navbar.js
+++ demo-app/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import {Link} from 'react-router-dom';
+import classes from './Navbar.module.css';
 
 function Navbar()
 {
-    return <header>
-        <h1><Link to='/'>ACM Cyber</Link></h1>
+    return <header className={classes.header}>
+        <h1 className={classes.logo}><Link to='/'>ACM Cyber</Link></h1>
         <nav>
             <ul>
                 <li><Link to='/challenges'>Challenges</Link></li>
