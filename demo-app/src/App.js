48aaca0eafe6064e71cbea6bd0346ad4e426ca86
Stephen Kelman <stephenrkelman@gmail.com>
2021-12-20 20:57:07 -0800

    Step 2: add some relevant content

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,6 +1,14 @@
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <h1>Challenges</h1>
+      <div className='chall'> {/* note: since 'class' is a reserved react term, we use className */}
+        <h2>Challenge 1</h2>
+        <div className='actions'>
+          <button className='button'>view</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
