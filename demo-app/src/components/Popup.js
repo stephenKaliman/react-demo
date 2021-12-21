9032ab64b5c734f402ab21691922a56c09f8c5c9
Stephen Kelman <stephenrkelman@gmail.com>
2021-12-20 21:51:53 -0800

    Step 5: annoying challenge info popup

---
+++ demo-app/src/components/Popup.js
@@ -0,0 +1,11 @@
+function Popup() {
+    return(
+        <div className='popup'>
+            <p>Challenge description blah blah blah</p>
+            <button className='button btn--alt'>Close</button>
+            <button className='button'>Submit</button>
+        </div>
+    );
+}
+
+export default Popup;
