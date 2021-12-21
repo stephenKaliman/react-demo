33ade25126dc26410abd2fc59b25d7d83ddb773c
Stephen Kelman <stephenrkelman@gmail.com>
2021-12-20 21:08:01 -0800

    Step 3.1: component working!... sort of

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -4,24 +4,9 @@ function App() {
   return (
     <div>
       <h1>Challenges</h1>
-      <div className='chall'> {/* note: since 'class' is a reserved react term, we use className */}
-        <h2>Challenge 1</h2>
-        <div className='actions'>
-          <button className='button'>view</button>
-        </div>
-      </div>
-      <div className='chall'> 
-        <h2>Challenge 2</h2>
-        <div className='actions'>
-          <button className='button'>view</button>
-        </div>
-      </div>
-      <div className='chall'> 
-        <h2>Challenge 3</h2>
-        <div className='actions'>
-          <button className='button'>view</button>
-        </div>
-      </div>
+      <Challenge />
+      <Challenge />
+      <Challenge />
     </div>
   );
 }
--- demo-app/src/components/Challenge.js
+++ demo-app/src/components/Challenge.js
@@ -1,6 +1,13 @@
 function Challenge()
 {
-
+    return (
+        <div className='chall'> {/* note: since 'class' is a reserved react term, we use className */}
+            <h2>Challenge 1</h2>
+            <div className='actions'>
+                <button className='button'>view</button>
+            </div>
+        </div>
+    );
 }
 
 export default Challenge;
